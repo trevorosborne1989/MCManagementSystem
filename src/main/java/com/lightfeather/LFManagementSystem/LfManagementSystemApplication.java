7b15c6f0e1f4a8fd7e272953c54db33b55ee63e9
tosborne <trevorosborne1989@gmail.com>
2022-06-24 23:12:14 -0600

    initial commit

---
+++ src/main/java/com/lightfeather/LFManagementSystem/LfManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.lightfeather.LFManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LfManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LfManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lightfeather/LFManagementSystem/LfManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lightfeather.LFManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LfManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
